8cfdff72c8b0546f4a9c2bda4e07a54046a501ab
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-03-20 16:06:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Write your code here!
+// let rem = document.getElementsByTagName("div")[0];
+// let rem2 = div.querySelector("main");
+// div.removeChild(rem2);
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Joan is the champion";
